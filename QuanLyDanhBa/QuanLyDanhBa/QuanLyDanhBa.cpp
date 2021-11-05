6912b5791d0e15be9c2d14f24a6b458198e202d8
Linhtd <linhltv9a2@gmail.com>
2021-11-05 19:10:02 +0700

    add code

--- QuanLyDanhBa/QuanLyDanhBa/QuanLyDanhBa.cpp
+++ QuanLyDanhBa/QuanLyDanhBa/QuanLyDanhBa.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 struct CONTACT { //Cây nhị phân tìm kiếm
-	SDT* ds_SDT[10]; //Lưu theo mảng động
+	SDT* ds_SDT; //Lưu theo mảng động
 	int soLuong; //Số số điện thoại mà một người sở hữu
 	string ten;
 	NHOM nhom;
@@ -40,7 +40,57 @@ struct NHOM { //Lưu theo danh sách liên kết đơn
 	CONTACT* ds_contact;
 	NHOM* next;
 };
-
+void TimNodeThayThe(CONTACT*& X, CONTACT*& Y) {
+	if (Y->right != NULL) {
+		TimNodeThayThe(X, Y->right);//Tìm Node phải nhất của cây con trái
+	}
+	else
+	{
+		X = Y;
+		Y = Y->left;
+	}
+}
+void XoaThongTin(CONTACT*& danhba)
+{
+	int sdtxoa;
+	cout << "\n\n\t\t Nhap so dien thoai can xoa: ";
+	cin >> sdtxoa;
+	if (danhba == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (danhba->ds_SDT->sdt != sdtxoa)
+		{
+			if (danhba->left != NULL)
+			{
+				XoaThongTin(danhba->left);
+			}
+			if (danhba->right != NULL)
+			{
+				XoaThongTin(danhba->right);
+			}
+		}
+		else
+		{
+			CONTACT* X = danhba;
+			if (danhba->left == NULL)//duyệt cập nhật mối liên kết của node cha của node cần xóa với node con của node cần xóa
+			{
+				danhba = danhba->right;
+			}
+			else if (danhba->right == NULL)
+			{
+				danhba = danhba->left;
+			}
+			else //node cần xóa có 2 con
+			{
+				TimNodeThayThe(X, danhba->left);
+			}
+			delete X;
+		}
+	}
+}
 void Menu(CONTACT*& danhba, DIACHI*& ds_DiaChi, NHOM*& ds_nhom) {
 	int luachon;
 	while (true) {
@@ -58,7 +108,7 @@ void Menu(CONTACT*& danhba, DIACHI*& ds_DiaChi, NHOM*& ds_nhom) {
 		
 		}
 		else if (luachon == 2) {
-
+			XoaThongTin(danhba);
 		}
 		else {
 			break;
