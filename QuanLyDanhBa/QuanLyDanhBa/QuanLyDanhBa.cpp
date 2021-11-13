f9cc4b75d352431d43ba2b44a405d3a2148fe4a9
Thim7 <93660448+Thim7@users.noreply.github.com>
2021-11-13 08:57:22 +0700

    fix: inform when you can not find phone number

--- QuanLyDanhBa/QuanLyDanhBa/QuanLyDanhBa.cpp
+++ QuanLyDanhBa/QuanLyDanhBa/QuanLyDanhBa.cpp
@@ -1,8 +1,6 @@
 ﻿#include<iostream>
 #include <stdio.h>
 #include <string>
-#include<fstream>
-#include<algorithm>
 using namespace std;
 
 struct DATE {
@@ -13,23 +11,30 @@ struct DATE {
 
 struct SDT {
 	int sdt;
+	//int maQuocGia;
 	string loai;
+};
+
+struct DIACHI { //Danh sách liên kết đơn
+	string tinh;
+	string nuoc;
+	DIACHI* next;
+};
+
+struct NHOM { //Lưu theo danh sách liên kết đơn
+	string ten;
+	NHOM* next;
 };
 
 struct DATA {
 	SDT  SDT;
 	string ten;
-	int nhom; //1: Gia dinh, 2: Ban be, 3: Cong viec
+	NHOM nhom;
 	string gioiTinh;
-	string diachi;
+	DIACHI diachi;
 	DATE ngaysinh;
 	string email;
 	string ghiChu;
-};
-
-struct NODE {
-	DATA data;
-	NODE* pNext;
 };
 
 struct CONTACT { //Cây nhị phân tìm kiếm
@@ -38,7 +43,6 @@ struct CONTACT { //Cây nhị phân tìm kiếm
 	CONTACT* left;
 };
 typedef CONTACT* TREE;
-
 
 //Hàm tách 3 số đầu của số điện thoại
 int tachSoDau(int sdt) {
@@ -78,6 +82,13 @@ void inputDateBirth(DATE& date) {
 	cin >> date.year;
 }
  
+//Hàm nhập địa chỉ
+void inputAddress(DIACHI& diaChi) {
+	while (getchar() != '\n');
+	cout << "\t\tTinh: ";
+	getline(cin, diaChi.tinh);
+}
+
 //Hàm nhập thông tin DATA
 void inputDATA(DATA& x) {
 	while (getchar() != '\n');
@@ -86,16 +97,8 @@ void inputDATA(DATA& x) {
 	cout << "\t\tGioi tinh: ";
 	getline(cin, x.gioiTinh);
 	inputPhoneNumber(x.SDT);
-	while (getchar() != '\n');
-	cout << "\t\tDia chi: ";
-	getline(cin, x.diachi);
+	inputAddress(x.diachi);
 	inputDateBirth(x.ngaysinh);
-	cout << "\t\tChon nhom";
-	cout << "\n\t\t\tNhap '1' chon Gia dinh";
-	cout << "\n\t\t\tNhap '2' chon Ban be";
-	cout << "\n\t\t\tNhap '3' chon Cong viec";
-	cout << "\n\t\tLua chon: ";
-	cin >> x.nhom;
 	while (getchar() != '\n');
 	string ktMail;
 	do {
@@ -127,41 +130,6 @@ void inputDATA(DATA& x) {
 	} while (ktNote != "y" && ktNote != "n");
 }
 
-//Khỏi tạo Node
-NODE* khoiTaoNODE_nv(DATA x) {
-	NODE* p = new NODE;
-	p->data = x;
-	p->pNext = NULL;
-	return p;
-}
-
-//Thêm NODE vào cuối danh sách
-void themNODE(NODE*& pHead, NODE* p) {
-	if (pHead == NULL) {
-		pHead = p;
-	}
-	else {
-		for (NODE* k = pHead; k != NULL; k = k->pNext) {
-			if (k->pNext == NULL) {
-				k->pNext = p;
-				return;
-			}
-		}
-	}
-}
-
-
-//Xử lí chuyển dữ liệu từ BFS sang DSLKD
-void BTStoLinkedList(TREE t, NODE*& pHead) {
-	if (t != NULL) {
-		NODE* p = khoiTaoNODE_nv(t->data);
-		themNODE(pHead, p);
-		BTStoLinkedList(t->right, pHead);
-		BTStoLinkedList(t->left, pHead);
-	}
-}
-
-
 //Hàm thêm data
 void insertData(TREE &contact, DATA data) {
 	if (contact == NULL) { //Nếu cây rỗng
@@ -187,17 +155,8 @@ void printDATA(DATA data) {
 	cout << "\n\t\tSo dien thoai: 0" << data.SDT.sdt;
 	cout << "\n\t\tNha mang: " << data.SDT.loai;
 	cout << "\n\t\tGioi tinh: " << data.gioiTinh;
-	cout << "\n\t\tDia chi: " << data.diachi;
+	cout << "\n\t\tDia chi: tinh " << data.diachi.tinh;
 	cout << "\n\t\tNgay sinh: " << data.ngaysinh.day << "/" << data.ngaysinh.month << "/" << data.ngaysinh.year;
-	if (data.nhom == 1) {
-		cout << "\n\t\tNhom: Gia dinh";
-	}
-	else if (data.nhom == 2) {
-		cout << "\n\t\tNhom: Ban be";
-	}
-	else if (data.nhom == 3) {
-		cout << "\n\t\tNhom: Cong viec";
-	}
 	if (data.email != "")
 		cout << "\n\t\tEmail: " << data.email;
 	if (data.ghiChu != "")
@@ -279,27 +238,6 @@ void deleteContact(TREE& contact, int sdtData) {
 	}
 }
 
-void XuatDanhBaTheoTen(NODE* pHead) {
-	for (NODE* k = pHead; k != NULL; k = k->pNext) {
-		for (NODE* h = k->pNext; h != NULL; h = h->pNext) {
-			string str1 = k->data.ten;
-			string str2 = h->data.ten;
-			transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
-			transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
-			if (str1.compare(str2) > 0) {
-				swap(k->data, h->data);
-			}
-		}
-	}
-
-	//Xuất họ tên nhân viên
-	for (NODE* k = pHead; k != NULL; k = k->pNext) {
-		printDATA(k->data);
-	}
-}
-
-
-//Hàm tìm kiếm số điện thoại theo tên
 void searchPhoneNumber(TREE& contact, int sdtData) 
 {
 	if (contact != NULL) {
@@ -308,50 +246,24 @@ void searchPhoneNumber(TREE& contact, int sdtData)
 		
 		if (data == Contact) 
 			printDATA(contact->data); // Nếu số nhập vào trùng thì in thông tin danh bạ
+		int m = data.size();
+		int n = Contact.size();
+		int count = 0;
 		
-		
-		else {
-			int m = data.size();
-			int n = Contact.size();
-			int count = 0;
-
-			for (int i = 0; i < m; i++) {
-				if (data[i] == Contact[i]) //Nếu từng chữ số nhập vào trùng với các chữ số trong cây nhị phân
-					count++; //  thì ta tăng biến đếm
-
-				if (count == data.size()) // Nếu biến đếm trùng với kích thước của số nhập vào
-					printDATA(contact->data); // thì ta in thông tin danh bạ
-			}
+		for (int i = 0; i < m; i++) {
+			if (data[i] == Contact[i]) //Nếu từng chữ số nhập vào trùng với các chữ số trong cây nhị phân
+				count++; //  thì ta tăng biến đếm
+			
+			if (count == data.size()) // Nếu biến đếm trùng với kích thước của số nhập vào
+				printDATA(contact->data); // thì ta in thông tin danh bạ
 		}
 		searchPhoneNumber(contact->left, sdtData); // Tiếp tục tìm kiếm các số trùng với số nhập vào ở bên trái
 		searchPhoneNumber(contact->right,sdtData); // và ở bên phải		
 	}
 }
-//Tim thong tin theo ten
-void SearchName(NODE*& data,string name,int x)
-{
-	if (data == NULL)
-		return;
-	NODE* p = data;
-	fflush(stdin);
-	string name2 = p->data.ten.substr(0,x);//  coppy n phan tu dau cua choi
-	if (name2.compare(name) == 0)// so sanh 2 chuoi
-	{
-		printDATA(p->data);
-	}
-	SearchName(p->pNext,name,x);
-}
-//Hàm xuất danh sách theo nhóm
-void printGroup(TREE contact, int maNhom) {
-	if (contact != NULL) {
-		if (contact->data.nhom == maNhom)
-			printDATA(contact->data);
-		printGroup(contact->left, maNhom);
-		printGroup(contact->right, maNhom);
-	}
-}
 
-void Menu(TREE& contact,NODE*& pHead) {
+
+void Menu(TREE& contact) {
 	int luachon;
 	while (true) {
 		system("cls");
@@ -360,13 +272,10 @@ void Menu(TREE& contact,NODE*& pHead) {
 		cout << "\n\n\t\t2. Xoa thong tin so dien thoai";
 		cout << "\n\n\t\t3. Xuat danh sach so dien thoai";
 		cout << "\n\n\t\t4. Tim thong tin danh ba theo so dien thoai";
-		cout << "\n\n\t\t5. Xuat danh sach so dien thoai theo ten";
-		cout << "\n\n\t\t6. Xuat danh sach theo nhom";
-		cout << "\n\n\t\t7. Tim ten";
 		cout << "\n\n\t\t0. Ket thuc";
 		cout << "\n\n\t\t:::::::::::::::::::::::::::::::::::::::END:::::::::::::::::::::::::::::::::::::::::::::::";
 
-		cout << "\n\n\t\t\t Chon chuc nang: ";
+		cout << "\n\n\t\t\t Lua chon: ";
 		cin >> luachon;
 
 		if (luachon == 1) {
@@ -396,89 +305,20 @@ void Menu(TREE& contact,NODE*& pHead) {
 			system("pause");
 		}
 		else if (luachon == 4) {
-			cout << "\n\t\t\tTIM KIEM SO DIEN THOAI";
 			int sdtData;
 			cout << "\n\t\tNhap so dien thoai ban can tim: ";
 			cin >> sdtData;
 			searchPhoneNumber(contact, sdtData);
 			system("pause");
 		}
-		else if (luachon == 5) {
-			cout << "\n\t\t\tXUAT DANH SACH THEO TEN";
-			BTStoLinkedList(contact, pHead);
-			XuatDanhBaTheoTen(pHead);
-			NODE* tam = NULL;
-			while (pHead != NULL) {
-				tam = pHead;
-				pHead = pHead->pNext;
-				delete tam;
-			}
-			system("pause");
-		}
-		else if (luachon == 6) {
-			cout << "\n\t\t\tXUAT DANH SACH THEO NHOM";
-			cout << "\n\t\tBan muon xuat danh sach nhom?";
-			cout << "\n\t\t\tNhap '1' chon Gia dinh";
-			cout << "\n\t\t\tNhap '2' chon Ban be";
-			cout << "\n\t\t\tNhap '3' chon Cong viec";
-			cout << "\n\t\tLua chon: ";
-			int maNhom;
-			cin >> maNhom;
-			printGroup(contact, maNhom);
-			system("pause");
-		}
-		else if (luachon == 7) {
-			cout << "\n\t\t\tTIM KIEM THEO TEN";
-			BTStoLinkedList(contact, pHead);
-			cout << "\n\t\t\tNhap ten can tim : ";
-			string name;
-			fflush(stdin);
-			while (getchar() != '\n');
-			getline(cin, name);
-			int leng = name.length();
-			SearchName(pHead,name,leng);
-			NODE* tam = NULL;
-			while (pHead != NULL) {
-				tam = pHead;
-				pHead = pHead->pNext;
-				delete tam;
-			}
-			system("pause");
-		}
-		else if (luachon == 0) {
+		else if(luachon == 0){
 			break;
 		}
 	}
 }
 
-//Doc mot thong tin
-void docData(ifstream &filein, DATA &data) {
-	getline(filein, data.ten, ',');
-	getline(filein, data.gioiTinh, ',');
-	getline(filein, data.diachi);
-	filein >> data.SDT.sdt >> data.nhom >> data.ngaysinh.day >> data.ngaysinh.month >> data.ngaysinh.year;
-}
-
-
-//Doc danh sach thong tin
-void docDanhSachData(ifstream &filein, TREE &contact) {
-	while (filein.eof() == false) {
-		DATA data;
-		docData(filein, data);
-		insertData(contact, data);
-	}
-}
 
 int main() {
 	TREE contact = NULL;
-	NODE* pHead = NULL;
-	ifstream filein;
-	filein.open("input.txt", ios_base::in);
-	if(filein.fail()) {
-		cout << "\n\t\t\tDuong dan khong hop le";
-		return 0;
-	}
-	docDanhSachData(filein, contact);
-	filein.close();
-	Menu(contact, pHead);
+	Menu(contact);
 }
